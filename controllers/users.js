61c847478eb3f29834e58f13528c49b2f7702b31
Marat <servrostov@gmail.com>
2022-07-10 14:52:46 +0300

    fif fix fix

--- controllers/users.js
+++ controllers/users.js
@@ -1,85 +1,85 @@
 const User = require('../models/user');
 
 const updateUserInfo = (req, res) => {
-	const { name, about } = req.body;
-	const userId = req.user._id;
-	User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true })
-		.then((data) => {
-			res.status(200).send(data);
-		})
-		.catch((error) => {
-			if (error.name === 'ValidationError') {
-				res.status(400).send({ message: 'Данные не прошли валидацию на сервере' });
-				return;
-			}
-			res.status(500).send({ message: `Ошибка сервера ${error}` });
-		});
+  const { name, about } = req.body;
+  const userId = req.user._id;
+  User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        res.status(400).send({ message: 'Данные не прошли валидацию на сервере' });
+        return;
+      }
+      res.status(500).send({ message: `Ошибка сервера ${error}` });
+    });
 };
 const updateUserAvatar = (req, res) => {
-	const { avatar } = req.body;
-	const userId = req.user._id;
-	User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true })
-		.then((data) => {
-			res.status(200).send(data);
-		})
-		.catch((error) => {
-			if (error.name === 'ValidationError') {
-				res.status(400).send({ message: 'Данные не прошли валидацию на сервере' });
-				return;
-			}
-			res.status(500).send({ message: `Ошибка сервера ${error}` });
-		});
+  const { avatar } = req.body;
+  const userId = req.user._id;
+  User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        res.status(400).send({ message: 'Данные не прошли валидацию на сервере' });
+        return;
+      }
+      res.status(500).send({ message: `Ошибка сервера ${error}` });
+    });
 };
 
 const createUser = (req, res) => {
-	const { name, about, avatar } = req.body;
-	User.create({ name, about, avatar })
-		.then((data) => {
-			res.status(200).send(data);
-		})
-		.catch((error) => {
-			if (error.name === 'ValidationError') {
-				res.status(400).send({ message: 'Данные не прошли валидацию на сервере' });
-				return;
-			}
-			res.status(500).send({ message: `Ошибка сервера ${error}` });
-		});
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        res.status(400).send({ message: 'Данные не прошли валидацию на сервере' });
+        return;
+      }
+      res.status(500).send({ message: `Ошибка сервера ${error}` });
+    });
 };
 const getUserById = (req, res) => {
-	const userId = req.params.id;
-	User.findById(userId)
-		.then((data) => {
-			if (!data) {
-				res.status(404).send({ message: `Пользователь с указанным id:${userId} не найден` });
-				return;
-			}
-			res.status(200).send(data);
-		})
-		.catch((error) => {
-			if (error.name === 'CastError') {
-				res.status(400).send({ message: `Неверно указан id:${userId}  ` });
-				return;
-			}
-			res.status(500).send({ message: `Ошибка сервера ${error}` });
-		});
+  const userId = req.params.id;
+  User.findById(userId)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({ message: `Пользователь с указанным id:${userId} не найден` });
+        return;
+      }
+      res.status(200).send(data);
+    })
+    .catch((error) => {
+      if (error.name === 'CastError') {
+        res.status(400).send({ message: `Неверно указан id:${userId}  ` });
+        return;
+      }
+      res.status(500).send({ message: `Ошибка сервера ${error}` });
+    });
 };
 
 const getUsers = (req, res) => {
-	User.find({})
-		.then((data) => {
-			res.status(200).send(data);
-		})
-		.catch((error) => {
-			if (error.name === 'CastError') {
-				res.status(404).send({ message: 'Пользователи  не созданы' });
-				return;
-			}
-			res.status(500).send({ message: `Ошибка сервера ${error}` });
-		});
+  User.find({})
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((error) => {
+      if (error.name === 'CastError') {
+        res.status(404).send({ message: 'Пользователи  не созданы' });
+        return;
+      }
+      res.status(500).send({ message: `Ошибка сервера ${error}` });
+    });
 };
 
 module.exports = {
-	createUser, getUserById, getUsers, updateUserInfo, updateUserAvatar,
+  createUser, getUserById, getUsers, updateUserInfo, updateUserAvatar,
 };
 
 // test
