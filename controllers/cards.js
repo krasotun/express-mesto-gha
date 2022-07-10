b7022bb63107ecc8709eee7a76241b3f386253b5
Marat <servrostov@gmail.com>
2022-07-10 15:22:10 +0300

    fix Исправил еще ошибки

--- controllers/cards.js
+++ controllers/cards.js
@@ -67,11 +67,15 @@ const removeLike = (req, res) => {
   const cardId = req.params.id;
   Card.findByIdAndUpdate(cardId, { $pull: { likes: req.user._id } }, { new: true })
     .then((data) => {
+      if (!data) {
+        res.status(404).send({ message: `Карточка с указанным id:${cardId} не найдена` });
+        return;
+      }
       res.status(200).send(data);
     })
     .catch((error) => {
       if (error.name === 'CastError') {
-        res.status(404).send({ message: 'Карточка не найдена' });
+        res.status(400).send({ message: 'Карточка не найдена' });
         return;
       }
       res.status(500).send({ message: `Ошибка сервера ${error}` });
