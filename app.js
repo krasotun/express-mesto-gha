b4b4d94838df91249e34449582c9f493d6bb409d
Marat <servrostov@gmail.com>
2022-07-10 16:08:03 +0300

    add Настроил роутер для ошибок

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const errorRouter = require('./routes/error');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -25,3 +26,4 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
+app.all('*', errorRouter);
