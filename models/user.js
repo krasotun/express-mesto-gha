15b9666d5476a3f96a2259ea4df7d8dfde3387f4
krasotun <servrostov@gmail.com>
2022-07-20 20:21:11 +0300

    add Добавил скрытие пароля в ответе

--- models/user.js
+++ models/user.js
@@ -28,6 +28,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false,
   },
   name: {
     type: String,
@@ -48,7 +49,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select(+'password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
