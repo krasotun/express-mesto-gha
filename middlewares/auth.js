c16d8da86acf96290c9d60fe45b935cab917be0d
krasotun <servrostov@gmail.com>
2022-07-23 21:11:49 +0300

    fix Исправил ошибку линтера

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,17 @@
 const jwt = require('jsonwebtoken');
+const AuthError = require('../errors/auth-error');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    throw new AuthError('Необходима авторизация');
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    throw new AuthError('Необходима авторизация');
   }
   req.user = payload;
   next();
