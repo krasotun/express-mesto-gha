0f27031f998802a988a7c0f65d5f7677684e10a2
krasotun <servrostov@gmail.com>
2022-07-23 19:47:16 +0300

    add исправил

--- models/user.js
+++ models/user.js
@@ -47,8 +47,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.statics.findUserByCredentials = function func(email, password) {
-  // return this.findOne({ email }).select('+password')
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
